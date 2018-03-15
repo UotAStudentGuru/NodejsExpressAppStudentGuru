3d139c1462b2e9fa933c783589ffbc2a249d3944
georgemakrakis <georgemakrakis88@gmail.com>
2018-03-15 21:19:27 +0200

    first commit

---
+++ NodejsExpressAppStudentGuru/models/expense.js
@@ -0,0 +1,13 @@
+﻿//Require Mongoose
+var mongoose = require('mongoose');
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var expense = new Schema({
+    type: String,
+    ammount: Number,
+    message: String
+});
+
+module.exports = mongoose.model('expense', expense);
---
+++ NodejsExpressAppStudentGuru/routes/index.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var expense = require('../models/expense');
+
+/* GET home page. */
+router.get('/', function (req, res)
+{       
+    res.render('index', { user: "Great User", title: 'Express' });
+
+    // Compile model from schema
+    //var expenseModel = mongoose.model('expense', expense);
+
+    // Create an instance of model SomeModel
+    var awesome_instance = new expense({ type: 'Bills', ammount: 3, message: "Koinoxrista" });
+
+    // Save the new model instance, passing a callback
+    awesome_instance.save(function (err) {
+        if (err)
+        {
+            return handleError(err);            
+        }
+        // saved!
+    });
+});
+
+module.exports = router;
